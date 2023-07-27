4884c10ca148452123d39fb1ac77df1f90236cf1
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-27 14:59:16 +0900

    Update Fabula's holy fire.c

--- Creating a problems/Fabula's sacred fire/Solve/Fabula's holy fire.c
+++ Creating a problems/Fabula's sacred fire/Solve/Fabula's holy fire.c
@@ -1,32 +1,32 @@
-//EOF 구현 해야함.
 //Fabula's torch
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+
 #define ALPHA 'A'
-int main(void) { int T;
-int N, flr, alpha;
-	scanf("%d", &T);
-	while (T--) {
-		scanf("%d", &N);
-		flr = 2*N-1;
-		for (int i = 0; i < flr; i++) { alpha = ALPHA+N;
-			for (int j = 0; j < flr; j++) {
-				printf("%c",
-					j >= abs(N - 1 - i) && j < flr - abs(N - 1 - i)
+int main(int argc, char** argv) {
+		int N;
+		while (scanf("%d", &N) != EOF) {
+			int flr; flr = 2 * N - 1;
+			for (int i = 0; i < flr; i++) {
+				int alpha; alpha = ALPHA + N;
+				for (int j = 0; j < flr; j++) {
+					printf("%c",
+						j >= abs(N - 1 - i) && j < flr - abs(N - 1 - i)
 						? (j < N ? --alpha : ++alpha)
-					: ' ');
-			} printf("\n");
-		}
-		for (int i = 0; i < flr; i++) { alpha = ALPHA+N;
-			for (int j = 0; j < flr; j++) {
-				printf("%c",
-					j <= abs(N-1-abs(N-1-i))
-						? --alpha : (j >= flr-abs(N-1-abs(N-1-i))
+						: ' ');
+				} printf("\n");
+			}
+			for (int i = 0; i < flr; i++) {
+				int alpha; alpha = ALPHA + N;
+				for (int j = 0; j < flr; j++) {
+					printf("%c",
+						j <= abs(N - 1 - abs(N - 1 - i))
+						? --alpha : (j >= flr - abs(N - 1 - abs(N - 1 - i))
 							? ++alpha : alpha));
-			} printf("\n");
+				} printf("\n");
+			}
+			printf("\n");
 		}
-		if(T) printf("\n");
-	}
+		printf("END.");
 return 0;}
