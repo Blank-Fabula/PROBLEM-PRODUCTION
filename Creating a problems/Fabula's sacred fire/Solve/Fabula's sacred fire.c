839aa6b5d3cf405fb28eb8854634e8f92454e1bf
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-27 15:09:33 +0900

    Update Fabula's sacred fire.c

--- Creating a problems/Fabula's sacred fire/Solve/Fabula's sacred fire.c
+++ Creating a problems/Fabula's sacred fire/Solve/Fabula's sacred fire.c
@@ -8,18 +8,18 @@ int main(int argc, char** argv) {
 		int N;
 		while (scanf("%d", &N) == true) {
 			int flr; flr = 2 * N - 1;
-			for (int i = 0; i < flr; i++) {
+			for (int i = 0; i < flr; ++i) {
 				int alpha; alpha = ALPHA + N;
-				for (int j = 0; j < flr; j++) {
+				for (int j = 0; j < flr; ++j) {
 					printf("%c",
 						j >= abs(N - 1 - i) && j < flr - abs(N - 1 - i)
 						? (j < N ? --alpha : ++alpha)
 						: ' ');
 				} printf("\n");
 			}
-			for (int i = 0; i < flr; i++) {
+			for (int i = 0; i < flr; ++i) {
 				int alpha; alpha = ALPHA + N;
-				for (int j = 0; j < flr; j++) {
+				for (int j = 0; j < flr; ++j) {
 					printf("%c",
 						j <= abs(N - 1 - abs(N - 1 - i))
 						? --alpha : (j >= flr - abs(N - 1 - abs(N - 1 - i))
